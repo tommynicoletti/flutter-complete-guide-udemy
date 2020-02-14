e204116f35ff97fce661352a15920afeadf0bdda
Tommaso Nicoletti <masinonicoletti@gmail.com>
2020-02-14 16:13:07 +0100

    Modal Bottom improvements

--- lib/widgets/user_transactions.dart
+++ lib/widgets/user_transactions.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import './new_transaction.dart';
 import './transaction_list.dart';
-import '../models/transaction.dart';
 
 class UserTransactions extends StatefulWidget {
   @override
@@ -9,34 +7,11 @@ class UserTransactions extends StatefulWidget {
 }
 
 class _UserTransactionsState extends State<UserTransactions> {
-  final List<Transaction> _userTransactions = [
-    Transaction(
-        id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
-    Transaction(
-        id: 't2',
-        title: 'Weekly Groceries',
-        amount: 12.44,
-        date: DateTime.now()),
-  ];
-
-  void _addNewTransaction(String txTitle, double txAmount) {
-    final newTx = Transaction(
-      title: txTitle,
-      amount: txAmount,
-      date: DateTime.now(),
-      id: DateTime.now().toString(),
-    );
-    setState(() {
-      _userTransactions.add(newTx);
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        NewTransaction(this._addNewTransaction),
-        TransactionList(this._userTransactions),
+        TransactionList([]),
       ],
     );
   }
